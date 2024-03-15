9869440fe927eddd586812e84a7378838fccc89b
Roman <rm388687@gmail.com>
2024-03-15 12:04:40 +0200

    lessonJS_2

---
+++ game-1.js
@@ -0,0 +1,25 @@
+// alert ("Привіт 🙋‍♂️, зірпаємо в гру ?")
+
+// alert("Правила прості потрібно вгадати число від 1 до 10")
+
+// let willPlay = confirm("Готовий ?")
+
+// const randomNumber = Math.floor(Math.random() * 10) + 1;
+// let win = randomNumber;
+
+// if (willPlay == false) {
+//     alert("Можливо, в інший раз. До побачення!")
+// }else{ numeric  = prompt ("Впиши число 🙃")}
+
+
+
+// if (randomNumber == numeric ) 
+
+//   {
+//     alert ("💥💥💥💥💥Виграв 💥💥💥💥, правельне число ")
+//  }
+//  else {  alert ("Спробуй іншим разом 🥲, правельне число  " )}
+
+//  alert("Дякую за гру ")
+
+ 
